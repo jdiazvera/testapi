31bda5f8a92ce9b32344d7e968dfb17c000ba063
Ali Corrales <alicorrales2013@gmail.com>
2022-07-27 15:49:18 -0400

    implementation of the connection to DB and ready the test of that

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
 import express from "express";
 import path from "path";
-
+import { connectDB } from "./util/connectDB";
+import * as dotenv from "dotenv";
 import { loadApiEndpoints } from "./controllers/api";
 
 // Create Express server
 const app = express();
-
+dotenv.config();
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 })
 );
+
+connectDB()
+
 
 loadApiEndpoints(app);
 
---
+++ test/connectDb.spec.ts
@@ -0,0 +1,9 @@
+import  request  from "supertest";
+import { connectDB } from "../src/util/connectDB";
+
+describe("Connection to Database", () => {
+    it("should connect to the database", async () => {
+        const response = await connectDB()
+        expect(response).toEqual("myDele")
+    })
+})
