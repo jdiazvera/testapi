479dc5a43f3b36fb35d8ffb91f547e161d8ef76f
Ali Corrales <alicorrales2013@gmail.com>
2022-07-27 16:02:23 -0400

    Implementation of logs

--- src/util/connectDB.ts
+++ src/util/connectDB.ts
@@ -1,8 +1,10 @@
 import { mongoose } from "@typegoose/typegoose";
+import { logger } from "./logger";
 
 export async function connectDB() {
     const db = await mongoose.connect("mongodb+srv://mongo:123...%40bcD.@cluster0.nsrarlo.mongodb.net/myDele");
     console.log("database is connected", db.connection.db.databaseName)
+    const log = logger.info("database is connected")
     const value = db.connection.db.databaseName
     return value
 } 
