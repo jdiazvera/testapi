873224e61e6724537aacdc1f3f93a6cba71b0896
Ali Corrales <alicorrales2013@gmail.com>
2022-07-28 14:21:15 -0400

    test1

--- test/connectDb.spec.ts
+++
@@ -1,8 +0,0 @@
-import { connectDB } from "../src/util/connectDB";
-
-describe("Connection to Database", () => {
-  it("should connect to the database", async () => {
-    const response = await connectDB();
-    expect(response).toEqual("myDele");
-  });
-});
