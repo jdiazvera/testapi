4a8c3b8d6f397ee14fda00d4140bdc6ea6bcc2e2
Ali Corrales <alicorrales2013@gmail.com>
2022-07-28 17:27:49 -0400

    delete log

--- src/util/connectDB.ts
+++ src/util/connectDB.ts
@@ -4,7 +4,6 @@ export async function connectDB() {
   const db = await mongoose.connect(
     "mongodb+srv://mongo:123...%40bcD.@cluster0.nsrarlo.mongodb.net/myDele"
   );
-  console.log("database is connected", db.connection.db.databaseName);
   const conn = db.connection.db.databaseName;
   return conn;
 }
