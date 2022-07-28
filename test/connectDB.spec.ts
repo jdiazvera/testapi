7e3a41e1e24dd00f42843817b4df298d8298f274
Ali Corrales <alicorrales2013@gmail.com>
2022-07-28 18:01:38 -0400

    Julio la mano magica

--- test/connectDB.spec.ts
+++ test/connectDB.spec.ts
@@ -2,7 +2,8 @@ import { connectDB } from "../src/util/connectDB";
 
 describe("Connection to Database", () => {
   it("should connect to the database", async () => {
-    const response = await connectDB();
-    expect(response).toEqual("myDele");
+    await connectDB().then((response) => {
+      expect(response).toEqual("myDele");
+    });
   });
 });
