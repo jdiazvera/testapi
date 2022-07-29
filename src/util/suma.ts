b7ae3988e56530b9d497d44e8ed207fead27eb60
Ali Corrales <alicorrales2013@gmail.com>
2022-07-29 10:32:08 -0400

    Finish resolution with out DB test

--- src/util/suma.ts
+++
@@ -1,3 +0,0 @@
-export function suma(a: number, b: number) {
-  return a + b;
-}
--- test/testExample.spec.ts
+++
@@ -1,8 +0,0 @@
-import { suma } from "../src/util/suma";
-
-describe("util/suma", () => {
-  it("should sum", () => {
-    const sum = suma(4, 5);
-    expect(sum).toEqual(9);
-  });
-});
